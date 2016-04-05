bd51e4be97d6e3aed13a51aa2f85ea9b4ce8507a
Joonas Vali <joonas.vali@zeroturnaround.com>
2016-04-05 19:30:41 +0300

    fix runners linebreaks for unix

---
+++ bin/unix/evolution/run-evolution.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+export MIRRORS_HOME=../..
+java -jar -Djava.library.path=./$MIRRORS_HOME/sl-libs/ -Xmx2048M ./$MIRRORS_HOME/lib/mirrors.jar evolution ./$MIRRORS_HOME/bin/evolution/evolution.unix.properties
