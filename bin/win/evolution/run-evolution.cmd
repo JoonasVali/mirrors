1bfc3bf55ed277de471fe420f7dda144cd74af53
Joonas Vali <joonas.wali@gmail.com>
2016-04-05 19:30:41 +0300

    fix runners linebreaks for unix

---
+++ bin/win/evolution/run-evolution.cmd
@@ -0,0 +1,3 @@
+@echo off
+SET MIRRORS_HOME=../..
+java -jar -Djava.library.path=./%MIRRORS_HOME%/sl-libs/ -Xmx2048M ./%MIRRORS_HOME%/lib/mirrors.jar evolution ./%MIRRORS_HOME%/bin/evolution/evolution.win.properties
