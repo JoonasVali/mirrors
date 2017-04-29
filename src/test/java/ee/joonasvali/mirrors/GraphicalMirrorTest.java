29f5d77910bdab9b82fee1f0e71c108168691d8c
Joonas Vali <joonas.vali@zeroturnaround.com>
2017-04-29 16:10:23 +0300

    first graphical test

--- src/test/java/ee/joonasvali/mirrors/GraphicalMirrorTest.java
+++ src/test/java/ee/joonasvali/mirrors/GraphicalMirrorTest.java
@@ -13,12 +13,23 @@ import java.lang.reflect.InvocationTargetException;
  */
 public class GraphicalMirrorTest {
   @Test
-  public void testSomething() throws InvocationTargetException, InterruptedException {
+  public void reflectorTest() throws InvocationTargetException, InterruptedException {
     EnvironmentBuilder builder = () -> {
       Environment environment = new Environment();
       environment.addObject(new Reflector(350, 250, 0 , 50, 50));
+
       environment.addObject(new Light(300, 200, 35, 1, 100, 0.1));
-      environment.addObject(new Light(50, 50, 35, 1, 100, 0.1));
+      environment.addObject(new Light(150, 150, 35, 1, 100, 0.1));
+
+      environment.addObject(new Light(500, 100, 135, 1, 100, 0.1));
+      environment.addObject(new Light(510, 100, 134, 1, 100, 0.1));
+
+      environment.addObject(new Light(480, 230, 168, 1, 100, 0.1));
+      environment.addObject(new Light(480, 230, 140, 1, 100, 0.1));
+
+      environment.addObject(new Light(350, 350, 280, 1, 100, 0.1));
+      environment.addObject(new Light(440, 350, 220, 1, 100, 0.1));
+
       return environment;
     };
     EnvironmentController controller = new DemoEnvironmentController(builder);
