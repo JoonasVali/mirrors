67f579141d00282ef7a707b9dccb69f494a61adb
Joonas Vali <joonas.vali@zeroturnaround.com>
2017-04-30 13:02:28 +0300

    add triangular reflectors

--- src/main/java/ee/joonasvali/mirrors/scene/genetic/GeneUtil.java
+++ src/main/java/ee/joonasvali/mirrors/scene/genetic/GeneUtil.java
@@ -3,6 +3,7 @@ package ee.joonasvali.mirrors.scene.genetic;
 import ee.joonasvali.mirrors.scene.genetic.impl.AcceleratorGene;
 import ee.joonasvali.mirrors.scene.genetic.impl.BenderGene;
 import ee.joonasvali.mirrors.scene.genetic.impl.ReflectorGene;
+import ee.joonasvali.mirrors.scene.genetic.impl.TriangleReflectorGene;
 
 import java.util.Random;
 
@@ -27,7 +28,7 @@ public class GeneUtil {
 
   public static Gene generateGene(Random rng, double maxX, double maxY) {
     Gene gene;
-    int type = (int)(rng.nextDouble() * 3);
+    int type = (int)(rng.nextDouble() * 4);
     double x = getRandom(rng, 0, maxX);
     double y = getRandom(rng, 0, maxY);
     if(type == 0) {
@@ -38,6 +39,14 @@ public class GeneUtil {
       double width = getRandom(rng, 10, 30);
       double height = getRandom(rng, 10, 30);
       gene = new ReflectorGene(x, y, width, height);
+    } else if(type == 2){
+      double x1 = getRandom(rng, 10, 40);
+      double x2 = getRandom(rng, 10, 40);
+      double x3 = getRandom(rng, 10, 40);
+      double y1 = getRandom(rng, 10, 40);
+      double y2 = getRandom(rng, 10, 40);
+      double y3 = getRandom(rng, 10, 40);
+      gene = new TriangleReflectorGene(x, y, x1, y1, x2, y2, x3, y3);
     } else {
       double radius = getRandom(rng, 10, 30);
       double acceleration = rng.nextDouble() / 10 - 0.05;
