40a20d2698e076a2cad7bba915bee98f4abe2de7
Joonas Vali <joonas.wali@gmail.com>
2019-04-01 08:08:46 +0300

    preparing to make evolution directories for convenience and separation of config files. (wip)

--- bin/unix/evolution/run-evolution.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-export MIRRORS_HOME=../..
-java -jar -Xmx2048M ./$MIRRORS_HOME/lib/mirrors.jar evolution ./$MIRRORS_HOME/bin/evolution/evolution.unix.properties
--- bin/win/load.cmd
+++ bin/win/load.cmd
@@ -1,3 +1,3 @@
 @echo off
-SET MIRRORS_HOME=../..
+SET MIRRORS_HOME=..
 java -jar -Xmx512M ./%MIRRORS_HOME%/lib/mirrors.jar load
