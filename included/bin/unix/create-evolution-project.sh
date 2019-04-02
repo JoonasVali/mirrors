e76a0a426f87e3be7b5fc7c75ec2da40ee0119c0
Joonas Vali <joonas.wali@gmail.com>
2019-04-02 22:05:33 +0300

    more improvements

---
+++ included/bin/unix/create-evolution-project.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+java -jar -Xmx512M ../lib/mirrors.jar create-evolution-gui
--- src/main/java/ee/joonasvali/mirrors/Launcher.java
+++ src/main/java/ee/joonasvali/mirrors/Launcher.java
@@ -3,23 +3,12 @@ package ee.joonasvali.mirrors;
 import ee.joonasvali.mirrors.command.CreateEvolutionGUI;
 import ee.joonasvali.mirrors.command.EvolutionDirectoryCreator;
 import ee.joonasvali.mirrors.command.GUIFileChooser;
+import ee.joonasvali.mirrors.command.RunEvolution;
 import ee.joonasvali.mirrors.command.RunRandomScene;
-import ee.joonasvali.mirrors.scene.EnvironmentBuilder;
-import ee.joonasvali.mirrors.scene.genetic.Genepool;
-import ee.joonasvali.mirrors.scene.genetic.GeneticEnvironmentBuilder;
-import ee.joonasvali.mirrors.scene.genetic.impl.LoaderGenepoolProvider;
-import ee.joonasvali.mirrors.scene.genetic.util.SerializationUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.Optional;
 
 /**
  * @author Joonas Vali April 2016
@@ -29,6 +18,12 @@ public class Launcher {
 
   public static void main(String[] args) throws InvocationTargetException, InterruptedException {
     EnvironmentController env = getEnvironment(args);
+
+    if (env == null) {
+      log.debug("EnvironmentController not present. Exiting.");
+      System.exit(0);
+    }
+
     WindowController controller = new WindowController(env);
     controller.launch();
   }
@@ -47,7 +42,7 @@ public class Launcher {
 
     if (arg.equals("evolution")) {
       log.info("Launching Evolution mode.");
-      return evolution(args);
+      return RunEvolution.evolution(args);
     }
 
     if (arg.equals("create-evolution")) {
@@ -61,36 +56,6 @@ public class Launcher {
     }
 
     log.error("Arguments didn't match anything we know how to parse.");
-    return null;
-  }
-
-  private static EnvironmentController evolution(String[] args) {
-    if (args.length < 2) {
-      log.error("expected second parameter to be the file pointing to evolution.properties");
-      return null;
-    }
-
-    Path evolutionProperties = Paths.get(args[1]).toAbsolutePath();
-    Path evolutionDirectory = evolutionProperties.getParent();
-    Path evolutionFile = evolutionDirectory.resolve(EvolutionController.POPULATIONS_FILE_NAME);
-    Collection<Genepool> seedPopulation = Collections.emptyList();
-
-    if (Files.exists(evolutionFile)) {
-      log.info("Loaded existing population from evolution file: " + evolutionFile);
-      try {
-        seedPopulation = SerializationUtil.deserializePopulation(evolutionFile);
-      } catch (IOException e) {
-        log.error("Unable to read evolution file.");
-        throw new RuntimeException(e);
-      }
-    }
-
-    EvolutionController controller = new EvolutionController(evolutionDirectory);
-    Optional<Genepool> winner = controller.runEvolution(seedPopulation);
-    if (winner.isPresent()) {
-      EnvironmentBuilder builder = new GeneticEnvironmentBuilder(new LoaderGenepoolProvider(winner.get()));
-      return new DemoEnvironmentController(builder);
-    }
     return null;
   }
 }
