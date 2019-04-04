bf8f9b8d9d08927efb883d3f01ed95df84cdbd75
Joonas Vali <joonas.wali@gmail.com>
2019-04-04 23:22:41 +0300

    disable other producers by default

--- src/main/java/ee/joonasvali/mirrors/EvolutionPropertyLoader.java
+++ src/main/java/ee/joonasvali/mirrors/EvolutionPropertyLoader.java
@@ -8,6 +8,22 @@ import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Properties;
+import java.util.function.Function;
+
+import static ee.joonasvali.mirrors.DefaultEvolutionPropertyValues.ACCELERATORS_ENABLED;
+import static ee.joonasvali.mirrors.DefaultEvolutionPropertyValues.BENDERS_ENABLED;
+import static ee.joonasvali.mirrors.DefaultEvolutionPropertyValues.BOTTOM_PRODUCER_ENABLED;
+import static ee.joonasvali.mirrors.DefaultEvolutionPropertyValues.CONCURRENT;
+import static ee.joonasvali.mirrors.DefaultEvolutionPropertyValues.ELITES;
+import static ee.joonasvali.mirrors.DefaultEvolutionPropertyValues.GENE_ADDITION_RATE;
+import static ee.joonasvali.mirrors.DefaultEvolutionPropertyValues.GENE_DELETION_RATE;
+import static ee.joonasvali.mirrors.DefaultEvolutionPropertyValues.GENE_MUTATION_RATE;
+import static ee.joonasvali.mirrors.DefaultEvolutionPropertyValues.KEEP_MACHINE_ALIVE;
+import static ee.joonasvali.mirrors.DefaultEvolutionPropertyValues.MIDDLE_PRODUCER_ENABLED;
+import static ee.joonasvali.mirrors.DefaultEvolutionPropertyValues.REFLECTORS_ENABLED;
+import static ee.joonasvali.mirrors.DefaultEvolutionPropertyValues.REPELLENTS_ENABLED;
+import static ee.joonasvali.mirrors.DefaultEvolutionPropertyValues.TARGET_FITNESS;
+import static ee.joonasvali.mirrors.DefaultEvolutionPropertyValues.TOP_PRODUCER_ENABLED;
 
 public class EvolutionPropertyLoader {
   private final static Logger log = LoggerFactory.getLogger(EvolutionPropertyLoader.class);
@@ -22,18 +38,32 @@ public class EvolutionPropertyLoader {
       }
 
       properties = new EvolutionProperties(
-          Integer.parseInt(props.getProperty("elites", String.valueOf(DefaultEvolutionPropertyValues.ELITES))),
-          Integer.parseInt(props.getProperty("concurrent", String.valueOf(DefaultEvolutionPropertyValues.CONCURRENT))),
-          Integer.parseInt(props.getProperty("target.fitness", String.valueOf(DefaultEvolutionPropertyValues.TARGET_FITNESS))),
-          Boolean.parseBoolean(props.getProperty("keep.machine.alive", String.valueOf(DefaultEvolutionPropertyValues.KEEP_MACHINE_ALIVE))),
-          Double.parseDouble(props.getProperty("gene.mutation.rate", String.valueOf(DefaultEvolutionPropertyValues.GENE_MUTATION_RATE))),
-          Double.parseDouble(props.getProperty("gene.deletion.rate", String.valueOf(DefaultEvolutionPropertyValues.GENE_DELETION_RATE))),
-          Double.parseDouble(props.getProperty("gene.addition.rate", String.valueOf(DefaultEvolutionPropertyValues.GENE_ADDITION_RATE))),
-          Boolean.parseBoolean(props.getProperty("reflectors.enabled", String.valueOf(DefaultEvolutionPropertyValues.REFLECTORS_ENABLED))),
-          Boolean.parseBoolean(props.getProperty("benders.enabled", String.valueOf(DefaultEvolutionPropertyValues.BENDERS_ENABLED))),
-          Boolean.parseBoolean(props.getProperty("accelerators.enabled", String.valueOf(DefaultEvolutionPropertyValues.ACCELERATORS_ENABLED))),
-          Boolean.parseBoolean(props.getProperty("repellents.enabled", String.valueOf(DefaultEvolutionPropertyValues.REPELLENTS_ENABLED)))
+          getProperty(props,"elites", Integer::parseInt, ELITES),
+          getProperty(props,"concurrent", Integer::parseInt, CONCURRENT),
+          getProperty(props,"target.fitness", Integer::parseInt, TARGET_FITNESS),
+          getProperty(props,"keep.machine.alive", Boolean::parseBoolean, KEEP_MACHINE_ALIVE),
+          getProperty(props,"gene.mutation.rate", Double::parseDouble, GENE_MUTATION_RATE),
+          getProperty(props,"gene.deletion.rate", Double::parseDouble, GENE_DELETION_RATE),
+          getProperty(props,"gene.addition.rate", Double::parseDouble, GENE_ADDITION_RATE),
+          getProperty(props,"reflectors.enabled", Boolean::parseBoolean, REFLECTORS_ENABLED),
+          getProperty(props,"benders.enabled", Boolean::parseBoolean, BENDERS_ENABLED),
+          getProperty(props,"accelerators.enabled", Boolean::parseBoolean, ACCELERATORS_ENABLED),
+          getProperty(props,"repellents.enabled", Boolean::parseBoolean, REPELLENTS_ENABLED)
       );
+
+      boolean topProducerEnabled = getProperty(
+          props,"top.producer.enabled", Boolean::parseBoolean, TOP_PRODUCER_ENABLED
+      );
+      boolean middleProducerEnabled = getProperty(
+          props,"middle.producer.enabled", Boolean::parseBoolean, MIDDLE_PRODUCER_ENABLED
+      );
+      boolean bottomProducerEnabled = getProperty(
+          props,"bottom.producer.enabled", Boolean::parseBoolean, BOTTOM_PRODUCER_ENABLED
+      );
+
+      properties.setTopProducerEnabled(topProducerEnabled);
+      properties.setMiddleProducerEnabled(middleProducerEnabled);
+      properties.setBottomProducerEnabled(bottomProducerEnabled);
     } catch (IOException e) {
       log.error("Can't read properties " + propertiesFile, e);
       throw new RuntimeException(e);
@@ -42,20 +72,17 @@ public class EvolutionPropertyLoader {
     return properties;
   }
 
-  public EvolutionProperties createDefaultEvolutionProperties() {
-    return new EvolutionProperties(
-        DefaultEvolutionPropertyValues.ELITES,
-        DefaultEvolutionPropertyValues.CONCURRENT,
-        DefaultEvolutionPropertyValues.TARGET_FITNESS,
-        DefaultEvolutionPropertyValues.KEEP_MACHINE_ALIVE,
-        DefaultEvolutionPropertyValues.GENE_MUTATION_RATE,
-        DefaultEvolutionPropertyValues.GENE_ADDITION_RATE,
-        DefaultEvolutionPropertyValues.GENE_DELETION_RATE,
-        DefaultEvolutionPropertyValues.REFLECTORS_ENABLED,
-        DefaultEvolutionPropertyValues.BENDERS_ENABLED,
-        DefaultEvolutionPropertyValues.ACCELERATORS_ENABLED,
-        DefaultEvolutionPropertyValues.REPELLENTS_ENABLED
-    );
+  private <T> T getProperty(Properties props, String key, Function<String, T> parseFunction, T defaultVal) {
+    String value = props.getProperty(key);
+    if (value == null) {
+      return defaultVal;
+    }
+    try {
+      return parseFunction.apply(value);
+    } catch (Exception e) {
+      log.error("Unable to parse value " + value + " for key " + key, e);
+      return defaultVal;
+    }
   }
 
 
