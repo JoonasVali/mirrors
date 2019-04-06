57884f5cb75dbe3fcd16115f3bdab2f8c047dea6
Joonas Vali <joonas.wali@gmail.com>
2019-04-06 11:18:45 +0300

    replace some math function with prebuilt safer ones.

--- src/main/java/ee/joonasvali/mirrors/graphics/Util.java
+++ src/main/java/ee/joonasvali/mirrors/graphics/Util.java
@@ -5,7 +5,7 @@ import java.awt.*;
 
 public class Util {
   public static double getDistance(double x1, double y1, double x2, double y2) {
-    return Math.sqrt(Math.pow(x1 - x2, 2) + Math.pow(y1 - y2, 2));
+    return Math.hypot(x1 - x2, y1 - y2);
   }
 
   public static double getAngleDiff(double first, double second) {
